eb351514dac9b422bb4a453453c801668cba7f6e
Santiago Ríos <santiago.riosv@gmail.com>
2018-06-08 15:25:10 -0500

    Inicio del proyecto galeria angular+firebase storage

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAbA_APdH3A_my92-BxmNozBAkP6QUMfOQ",
+    authDomain: "galeria-9671a.firebaseapp.com",
+    databaseURL: "https://galeria-9671a.firebaseio.com",
+    projectId: "galeria-9671a",
+    storageBucket: "galeria-9671a.appspot.com",
+    messagingSenderId: "23313588599"
+
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAbA_APdH3A_my92-BxmNozBAkP6QUMfOQ",
+    authDomain: "galeria-9671a.firebaseapp.com",
+    databaseURL: "https://galeria-9671a.firebaseio.com",
+    projectId: "galeria-9671a",
+    storageBucket: "galeria-9671a.appspot.com",
+    messagingSenderId: "23313588599"
+  }
+};
